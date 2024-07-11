824ee4bce0108ac0df6e06c8468e6c397780b51c
vizssouza <vini.nelso@gmail.com>
2024-07-11 15:55:33 -0300

    Atualizando php

--- enviar.php
+++ enviar.php
@@ -18,19 +18,17 @@ $email = $_POST['email'];
 $telefone = $_POST['telefone'];
 
 // Preparar e executar a inserção
-$sql = "INSERT INTO dados (nome, email, telefone) VALUES ($nome, $email, $telefone)";
+$sql = "INSERT INTO dados (nome, email, telefone) VALUES ('$nome', '$email', '$telefone')";
 $stmt = $conn->prepare($sql);
 
 
 if ($stmt->execute()) {
     echo "Registro inserido com sucesso!";
     // Redireciona para o link desejado
-    header('Location: https://api.whatsapp.com/send?phone=558184338374&text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20do%20site%20e%20gostaria%20de%20alcan%C3%A7ar%20mais%20pessoas%20com%C2%A0o%C2%A0Tr%C3%A1fego%C2%A0Pago');
+    header('Location: https://api.whatsapp.com/send?phone=558189978445&text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20do%20site%20e%20gostaria%20de%20alcan%C3%A7ar%20mais%20pessoas%20com%20Tr%C3%A1fego%20Pago!');
 } else {
     echo "Erro ao inserir registro: " . $stmt->error;
 }
-
-next()
 
 // Fechar a conexão
 $stmt->close();
